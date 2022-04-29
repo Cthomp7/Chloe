983705f7186eff1fe71d49fa9f7912ad337b5a30
Cthomp7 <cthomp7@saic.edu>
2022-04-29 14:56:07 -0500

    added touch feature

--- drawing/public/sketch.js
+++ drawing/public/sketch.js
@@ -31,3 +31,18 @@ function setup() {
     fill(255);
     ellipse(mouseX, mouseY, 36, 36);
   }
+
+  function touchStarted() {
+    console.log('Sending: ' + mouseX + ',' + mouseY);
+
+    var data = {
+      x: mouseX,
+      y: mouseY
+    }
+
+    socket.emit('mouse', data);
+
+    noStroke();
+    fill(255);
+    ellipse(mouseX, mouseY, 36, 36);
+  }
